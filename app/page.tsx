7fbb88e3c8387cb2f27f6d41297380e4b5d4f12f
22emini <eminioluwaakinrinade716@example.com>
2025-05-26 12:03:46 +0100

    Add TypeScript configuration and type definitions for the project
    - Created a new tsconfig.json file with compiler options for TypeScript.
    - Added type definitions for various models including User, Companion, and Message types.
    - Defined enums for Subject and Message types to standardize usage across the application.
    - Introduced interfaces for creating and managing companions, users, and messages.

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      page
+      <Button >what up?</Button>
+      <Button variant="outline">what up?</Button>
+      <Button variant="destructive">what up?</Button>
+      <h1 className=''> what good mr west</h1>
+    </div>
+  )
+}
+
+export default page
---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images:{
+    domains: [
+      'cdn-icons-png.flaticon.com',
+      'hebbkx1anhila5yf.public.blob.vercel-storage.com'
+    ]
+  }
+};
+
+export default nextConfig;
